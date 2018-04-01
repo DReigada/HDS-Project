cb8ba4e9074a95dcaa7a084700aae68c3908f617
Radaeld <joao.serras@ist.utl.pt>
2018-04-02 00:11:54 +0100

    Register Integration Completed

--- hds-client/src/main/java/com/tecnico/sec/hds/client/commands/RegisterCommand.java
+++ hds-client/src/main/java/com/tecnico/sec/hds/client/commands/RegisterCommand.java
@@ -3,14 +3,24 @@ package com.tecnico.sec.hds.client.commands;
 import com.tecnico.sec.hds.client.Client;
 import io.swagger.client.ApiException;
 import io.swagger.client.model.RegisterRequest;
+import io.swagger.client.model.RegisterResponse;
+import io.swagger.client.model.Signature;
+
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
 
 public class RegisterCommand extends AbstractCommand {
   private static final String name = "register";
 
   @Override
-  public void doRun(Client client, String[] args) throws ApiException {
+  public void doRun(Client client, String[] args) throws ApiException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
     RegisterRequest request = new RegisterRequest().publicKey(client.key);
-    client.server.register(request);
+    Signature sign = new Signature().value(client.cryptoAgent.generateSignature(client.key.getValue()));
+    request.signature(sign);
+
+    RegisterResponse response = client.server.register(request);
+    System.out.println(response.getBla());
   }
 
   @Override
--- hds-server/src/main/java/com/tecnico/sec/hds/server/controllers/RegisterController.java
+++ hds-server/src/main/java/com/tecnico/sec/hds/server/controllers/RegisterController.java
@@ -1,6 +1,9 @@
 package com.tecnico.sec.hds.server.controllers;
 
 
+import com.tecnico.sec.hds.server.db.commands.exceptions.DBException;
+import com.tecnico.sec.hds.server.db.rules.RegisterRules;
+import com.tecnico.sec.hds.util.crypto.CryptoAgent;
 import io.swagger.api.RegisterApi;
 import io.swagger.api.RegisterApiController;
 import io.swagger.model.RegisterRequest;
@@ -13,21 +16,43 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.validation.Valid;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
+import java.security.spec.InvalidKeySpecException;
 
 @Controller
 public class RegisterController implements RegisterApi {
 
   private static final Logger log = LoggerFactory.getLogger(RegisterApiController.class);
+  private CryptoAgent cryptoAgent;
+  private RegisterRules registerRules;
+
+  public RegisterController() throws NoSuchAlgorithmException, IOException, InvalidKeySpecException {
+    cryptoAgent = new CryptoAgent("bank");
+    registerRules = new RegisterRules();
+  }
 
   @Override
   public ResponseEntity<RegisterResponse> register(@Valid @RequestBody RegisterRequest body) {
     String key = body.getPublicKey().getValue();
-    log.info("Got new public key: " + key);
+    String signature = body.getSignature().getValue();
+    log.info("Public key: " + key);
+    log.info("1n Signature: " + signature);
 
+    RegisterResponse response =  new RegisterResponse();
 
+    try {
+      cryptoAgent.verifySignature(key,signature);
+      registerRules.register(key);
+      response.addBlaItem("Registration Completed:").addBlaItem(key);
+    } catch (NoSuchAlgorithmException | InvalidKeyException | SignatureException e) {
+      response.addBlaItem("Registration Fail: Invalid Keys");
+    } catch (DBException e) {
+      response.addBlaItem("Registration Fail: Invalid Database, Try Later");
+    }
 
-    RegisterResponse response =
-        new RegisterResponse().addBlaItem("Hello").addBlaItem("user:").addBlaItem(key);
     return new ResponseEntity<>(response, HttpStatus.OK);
   }
 }
