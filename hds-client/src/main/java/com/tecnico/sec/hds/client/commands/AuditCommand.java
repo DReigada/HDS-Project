d179152cf653f72c2b9dd7f056c790586853397e
Radaeld <joao.serras@ist.utl.pt>
2018-04-04 22:42:45 +0100

    Audit Being Implemented

--- hds-client/src/main/java/com/tecnico/sec/hds/client/commands/AuditCommand.java
+++ hds-client/src/main/java/com/tecnico/sec/hds/client/commands/AuditCommand.java
@@ -3,6 +3,8 @@ package com.tecnico.sec.hds.client.commands;
 import com.tecnico.sec.hds.client.Client;
 import io.swagger.client.ApiException;
 import io.swagger.client.model.AuditRequest;
+import io.swagger.client.model.AuditResponse;
+import io.swagger.client.model.TransactionInformation;
 
 public class AuditCommand extends AbstractCommand {
   private static final String name = "audit";
@@ -11,7 +13,12 @@ public class AuditCommand extends AbstractCommand {
   public void doRun(Client client, String[] args) throws ApiException {
     AuditRequest auditRequest = new AuditRequest();
     auditRequest.publicKey(client.key);
-    client.server.audit(auditRequest);
+
+    AuditResponse auditResponse = client.server.audit(auditRequest);
+
+    for(TransactionInformation transactionInformation : auditResponse.getList()){
+
+    }
   }
 
   @Override
--- hds-server/src/main/java/com/tecnico/sec/hds/server/controllers/AuditController.java
+++ hds-server/src/main/java/com/tecnico/sec/hds/server/controllers/AuditController.java
@@ -1,22 +1,71 @@
 package com.tecnico.sec.hds.server.controllers;
 
+import com.tecnico.sec.hds.server.db.commands.exceptions.DBException;
+import com.tecnico.sec.hds.server.db.rules.AuditRules;
+import com.tecnico.sec.hds.server.domain.Transaction;
+import com.tecnico.sec.hds.util.crypto.CryptoAgent;
 import io.swagger.annotations.ApiParam;
 import io.swagger.api.AuditApi;
-import io.swagger.model.AuditRequest;
+import io.swagger.model.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.validation.Valid;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.List;
 
 /**
  * Created by jp_s on 3/12/2018.
  */
 @Controller
 public class AuditController implements AuditApi{
+  private AuditRules auditRules;
+  private CryptoAgent cryptoAgent;
+
+
+  public AuditController() throws NoSuchAlgorithmException, IOException, InvalidKeySpecException {
+    cryptoAgent = new CryptoAgent("bank");
+    auditRules = new AuditRules();
+  }
+
   @Override
-  public ResponseEntity<Void> audit(@ApiParam(value = "", required = true) @RequestBody @Valid AuditRequest body) {
+  public ResponseEntity<AuditResponse> audit(@ApiParam(required = true) @RequestBody @Valid AuditRequest body) {
+    String pubKey = body.getPublicKey().getValue();
+    AuditResponse auditResponse = new AuditResponse();
+    try {
+      List<Transaction> history = auditRules.audit(pubKey);
+      Signature sign = new Signature();
+
+      for (Transaction transaction : history){
+        Hash hash = new Hash();
+        hash.value(transaction.hash);
+        Signature signature = new Signature();
+        signature.value(transaction.signature);
+        TransactionInformation transactionInformation = new TransactionInformation();
+        transactionInformation.setTransID(transaction.transID);
+        transactionInformation.setSourceKey(transaction.sourceKey);
+        transactionInformation.setDestKey(transaction.destKey);
+        transactionInformation.setAmount("" + transaction.amount);
+        transactionInformation.setPending(transaction.pending);
+        transactionInformation.setReceive(transaction.receive);
+        transactionInformation.setSignature(signature);
+        transactionInformation.setHash(hash);
+        auditResponse.addListItem(transactionInformation);
+      }
+
+      sign.value(cryptoAgent.generateSignature(auditResponse.getList().toString()));
+      auditResponse.setSignature(sign);
+
+    } catch (DBException | NoSuchAlgorithmException | SignatureException | InvalidKeyException e) {
+      e.printStackTrace();
+    }
+
     return new ResponseEntity<>(HttpStatus.OK);
   }
 }
