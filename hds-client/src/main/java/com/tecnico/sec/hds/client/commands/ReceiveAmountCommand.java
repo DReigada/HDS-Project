01a0b40de3c00f37247784b07499988eb4a5515e
Diogo-Castilho <diogo@castilho.me>
2018-04-05 22:15:23 +0100

    ReceiveAmount Command partially done

--- hds-client/src/main/java/com/tecnico/sec/hds/client/commands/ReceiveAmountCommand.java
+++ hds-client/src/main/java/com/tecnico/sec/hds/client/commands/ReceiveAmountCommand.java
@@ -2,24 +2,48 @@ package com.tecnico.sec.hds.client.commands;
 
 import com.tecnico.sec.hds.client.Client;
 import io.swagger.client.ApiException;
+import io.swagger.client.model.Hash;
 import io.swagger.client.model.ReceiveAmountRequest;
+import io.swagger.client.model.ReceiveAmountResponse;
 import io.swagger.client.model.Signature;
 
+import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SignatureException;
+import java.security.spec.InvalidKeySpecException;
 
 public class ReceiveAmountCommand extends AbstractCommand {
   private static final String name = "receive_amount";
 
   @Override
-  public void doRun(Client client, String[] args) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, ApiException {
-    String message = "";
+  public void doRun(Client client, String[] args) throws IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidKeySpecException, SignatureException, ApiException {
+
+    Hash hash = new Hash();
+    hash.setValue(args[0]);
+    System.out.println(hash.getValue());
+
+    Signature signature = new Signature();
+
+    signature.setValue(client.cryptoAgent.generateSignature(client.key + hash.getValue()));
+
     ReceiveAmountRequest receiveAmountRequest = new ReceiveAmountRequest();
     receiveAmountRequest.publicKey(client.key);
-    Signature signature = new Signature().value(client.cryptoAgent.generateSignature(message));
+    receiveAmountRequest.setTransHash(hash);
     receiveAmountRequest.signature(signature);
-    client.server.receiveAmount(receiveAmountRequest);
+
+    ReceiveAmountResponse receiveAmountResponse = client.server.receiveAmount(receiveAmountRequest);
+
+    hash = receiveAmountResponse.getNewHash();
+
+    if (client.cryptoAgent.verifyBankSignature(hash.getValue() + receiveAmountResponse.getMessage(),
+      receiveAmountResponse.getSignature().getValue())) {
+      client.setLastHash(hash);
+      System.out.println(receiveAmountResponse.getMessage());
+    } else {
+      System.out.println("I caught you fake!!");
+    }
+
   }
 
   @Override
--- hds-client/src/main/java/com/tecnico/sec/hds/client/commands/SendAmountCommand.java
+++ hds-client/src/main/java/com/tecnico/sec/hds/client/commands/SendAmountCommand.java
@@ -14,7 +14,7 @@ public class SendAmountCommand extends AbstractCommand {
   private static final String name = "send_amount";
 
   @Override
-  public void doRun(Client client, String[] args) throws ApiException {
+  public void doRun(Client client, String[] args) throws ApiException, NoSuchAlgorithmException, SignatureException, InvalidKeyException, IOException, InvalidKeySpecException {
     PubKey destKey = new PubKey();
 
     destKey.setValue(args[0]);
@@ -23,35 +23,28 @@ public class SendAmountCommand extends AbstractCommand {
 
     Integer amount = Integer.parseInt(args[1]);
 
-
     Signature signature = new Signature();
 
+    signature.setValue(client.cryptoAgent.generateSignature(client.key.getValue() + destKey.getValue() + amount.toString() + lastHash.getValue()));
+    SendAmountRequest sendAmount = new SendAmountRequest();
+    sendAmount.setSourceKey(client.key);
+    sendAmount.setDestKey(destKey);
+    sendAmount.setAmount(amount);
+    sendAmount.setLastHash(lastHash);
+    sendAmount.setSignature(signature);
 
-    try {
-      signature.setValue(client.cryptoAgent.generateSignature(client.key.getValue() + destKey.getValue() + amount.toString() + lastHash.getValue()));
-      SendAmountRequest sendAmount = new SendAmountRequest();
-      sendAmount.setSourceKey(client.key);
-      sendAmount.setDestKey(destKey);
-      sendAmount.setAmount(amount);
-      sendAmount.setLastHash(lastHash);
-      sendAmount.setSignature(signature);
+    SendAmountResponse sendAmountResponse = client.server.sendAmount(sendAmount);
 
-      SendAmountResponse sendAmountResponse = client.server.sendAmount(sendAmount);
+    lastHash = sendAmountResponse.getNewHash();
 
-      lastHash = sendAmountResponse.getNewHash();
-
-      Signature bankSignature = sendAmountResponse.getSignature();
+    Signature bankSignature = sendAmountResponse.getSignature();
 
 
-      if (client.cryptoAgent.verifyBankSignature(lastHash.getValue() + sendAmountResponse.getMessage(), bankSignature.getValue())) {
-        client.setLastHash(lastHash);
-        System.out.println(sendAmountResponse.getMessage());
-      } else {
-        System.out.println("I caught you fake!!");
-      }
-
-    } catch (NoSuchAlgorithmException | SignatureException | InvalidKeyException | IOException | InvalidKeySpecException e) {
-      e.printStackTrace();
+    if (client.cryptoAgent.verifyBankSignature(lastHash.getValue() + sendAmountResponse.getMessage(), bankSignature.getValue())) {
+      client.setLastHash(lastHash);
+      System.out.println(sendAmountResponse.getMessage());
+    } else {
+      System.out.println("I caught you fake!!");
     }
 
   }
