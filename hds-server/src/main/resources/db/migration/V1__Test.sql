5d271117c881ab89548d900f6a0a337914988766
Daniel Reigada <daniel.reigada@gmail.com>
2018-04-28 17:37:00 +0100

    Remove balance from account and add receive hash to transaction

--- hds-server/src/main/resources/db/migration/V1__Test.sql
+++ hds-server/src/main/resources/db/migration/V1__Test.sql
@@ -1,18 +1,18 @@
 CREATE TABLE IF NOT EXISTS accounts (
-  publicKey VARCHAR PRIMARY KEY NOT NULL,
-  balance   BIGINT              NOT NULL
+  publicKey VARCHAR PRIMARY KEY NOT NULL
 );
 
 
 CREATE TABLE IF NOT EXISTS transactions (
-  transID   INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  sourceKey VARCHAR                            NOT NULL,
-  destKey   VARCHAR                            NOT NULL,
-  amount    BIGINT                             NOT NULL,
-  pending   BOOLEAN                            NOT NULL,
-  receive   BOOLEAN                            NOT NULL,
-  signature VARCHAR                            NOT NULL,
-  hash      VARCHAR                            NOT NULL,
+  transID      INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  sourceKey    VARCHAR                            NOT NULL,
+  destKey      VARCHAR                            NOT NULL,
+  amount       BIGINT                             NOT NULL,
+  pending      BOOLEAN                            NOT NULL,
+  receive      BOOLEAN                            NOT NULL,
+  signature    VARCHAR                            NOT NULL,
+  hash         VARCHAR                            NOT NULL,
+  receive_hash VARCHAR,
   FOREIGN KEY (sourceKey) REFERENCES accounts (publicKey),
   FOREIGN KEY (destKey) REFERENCES accounts (publicKey)
 );
